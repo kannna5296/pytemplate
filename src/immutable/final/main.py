f156a1bbaf74fce1a66eda118c1ef081a1d71142
kannna5296 <kannnahacks5296@gmail.com>
2025-07-08 14:41:20 +0000

    静的ツール追加

--- src/immutable/final/main.py
+++ src/immutable/final/main.py
@@ -1,14 +1,16 @@
 from typing import Final
 
-def main():
+def main() -> None:
     print("##### 実行はできる。が、静的ツールに怒ってもらえる話 #####")
-    final = Final()
+    final = FinalClass()
     print("変更前 " +str(final.count))
     final.count = 10
+    # mypy main.pyをすると、error: Cannot assign to final attribute "count"  [misc]
+    # 実行はできちゃう
     print("変更後 " +str(final.count))
 
-class Final:
-    count : Final = 1
+class FinalClass:
+    count : Final[int] = 1
 
 if __name__ == "__main__":
     main()
