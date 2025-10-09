25c5e249a805eb1a06866aeef4ae721ce0f20f4a
kannna5296 <kannnahacks5296@gmail.com>
2025-10-09 23:13:36 +0900

    multiple_id追加

---
+++ src/pandas/multiple_id.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+# 対象者の基礎データ
+# 年度ごとに対象者が決まる
+df = pd.DataFrame(data = [
+    [1, 1],
+    [2, np.nan],
+    [np.nan, 2],
+    [np.nan, np.nan],
+], columns=['id1', 'id2'])
+
+print(df)
+
+aa = df[["id1", "id2"]].notna()
+print()
+print(aa)
+bb = aa.any(axis=1)
+print()
+print(bb)
+cc = bb.all()
+print()
+print(cc)
