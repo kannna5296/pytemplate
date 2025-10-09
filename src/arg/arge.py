7dc504d48cf02193948f624e2192e18591f73c8f
kannna5296 <kannnahacks5296@gmail.com>
2025-10-09 14:29:39 +0000

    引数を食ってそれを出すだけ

---
+++ src/arg/arge.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import argparse
+from pathlib import Path
+
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='複数のCSVファイルを処理し、id列の存在をチェックします'
+    )
+    parser.add_argument(
+        'files',
+        nargs='+', #1個以上
+        help='処理するファイルの相対パス（複数指定可能）'
+    )
+
+    args = parser.parse_args()
+    print(args.files)
+
+if __name__ == '__main__':
+    main()
