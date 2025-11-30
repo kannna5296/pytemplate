8d3367d9fb21160e614c306f7c4183af6f6cea59
kannna5296 <kannnahacks5296@gmail.com>
2025-11-30 09:15:56 +0000

    defaultdict使ってみる

---
+++ src/4_dict/27-defaultdict.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+class Visits:
+    def __init__(self):
+        self.data = defaultdict(set)
+        print(self.data) #defaultdict(<class 'set'>, {}) ※{}はsetの意味
+
+    def add(self, country: str, city: str) -> None:
+        self.data[country].add(city)
+
+visits = Visits()
+visits.add("England", "Batch")
+visits.add("England", "London")
+visits.add("USA", "Hage")
+print(visits.data)
+
+
+fuga = {}
+print(type(fuga))
