a4e743bdff7c6a0c6f948b3bcdd1678dd4f2cc0c
kannna5296 <kannnahacks5296@gmail.com>
2025-11-30 04:55:25 +0000

    コピぺ

---
+++ src/abc433/d.py
@@ -0,0 +1,22 @@
+import bisect
+
+# コピペ〜〜
+
+n, m = map(int, input().split())
+list = list(map(int, input().split()))
+g = [[] for _ in range(11)] #空配列が11個ある配列(0桁から10桁それぞれ)
+print(g)
+for v in list:
+    #print(str(v), len(str(v)), v % m)
+    g[len(str(v))].append(v % m)
+print(g)
+for gg in g:
+    gg.sort()
+print(g)
+ans = 0
+for ai in list:
+    for k in range(1, 11):
+        ai = (ai * 10) % m
+        key = (m - ai) % m
+        ans += bisect.bisect_left(g[k], key + 1) - bisect.bisect_left(g[k], key)
+print(ans)
