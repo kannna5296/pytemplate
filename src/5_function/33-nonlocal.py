ee185d5faf951032f875f43569668b56c7ea9003
kannna5296 <kannnahacks5296@gmail.com>
2025-12-01 14:38:01 +0000

    コード書いてみた

---
+++ src/5_function/33-nonlocal.py
@@ -0,0 +1,17 @@
+def sort_priority(numbers, group):
+    found = False
+    def helper(x):
+        nonlocal found # これがない場合、6行目のTrueはスコープ外になるので、変更されない
+        if x in group:
+            found = True
+            return (0,x)
+        return (1,x)
+    numbers.sort(key=helper)
+    return found
+
+numbers = [8, 4, 1, 2, 5, 4, 7, 6]
+group = [2, 3, 5, 7]
+
+found = sort_priority(numbers, group)
+print(found)
+print(numbers)
