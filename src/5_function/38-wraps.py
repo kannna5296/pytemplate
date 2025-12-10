af1e7e25a58f40a0990b399506cd8aefc5079645
kannna5296 <kannnahacks5296@gmail.com>
2025-12-10 13:48:41 +0000

    38

---
+++ src/5_function/38-wraps.py
@@ -0,0 +1,26 @@
+from functools import wraps
+import pickle
+
+
+
+
+def trace(func):
+    @wraps(func) #wrapper関数を使うときは、functoolsを使うべきよ〜〜
+    def wrapper(*args, **kargs):
+        args_repr = repr(args)
+        kargs_repr = repr(kargs)
+        result = func(*args, **kargs)
+        print(f"{func.__name__}({args_repr}, {kargs_repr}) -> {result!r}")
+        return result
+    return wrapper
+
+@trace
+def fibonacci(n):
+    "n番目のfibonacci数を返す"
+    if n in (0, 1):
+        return n
+    return fibonacci(n - 2) + fibonacci(n - 1)
+
+fibonacci(4)
+#help(fibonacci)
+print(pickle.dumps(fibonacci))
