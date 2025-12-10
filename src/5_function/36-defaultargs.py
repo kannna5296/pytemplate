858273b5f9aedb2435f9e695c7de14db54c37315
kannna5296 <kannnahacks5296@gmail.com>
2025-12-10 13:17:01 +0000

    引数がミュータブル（=関数内部で変わるとき）な場合はデフォルトを関数定義の中で使っちゃダメ！！！

---
+++ src/5_function/36-defaultargs.py
@@ -0,0 +1,22 @@
+from time import sleep
+from datetime import datetime
+
+
+def log(message, time = datetime.now()):
+    print(f"{message}, {time}")
+
+
+log("こんにちは！")
+sleep(0.1)
+log("こんにちは！！") #同じ値しか出ない。 関数定義時にnowが1回呼び出されるだけだから
+# 引数がミュータブル（=関数内部で変わるとき）な場合はデフォルトを関数定義の中で使っちゃダメ！！！
+
+
+def log_time(message: str, time: datetime | None = None):
+    if time is None:
+        time = datetime.now()
+    print(f"{message}, {time}")
+
+log_time("ちゃんとこんにちは！")
+sleep(0.1)
+log_time("ちゃんとこんにちは！！") #違う値が出る。意図どおり。
