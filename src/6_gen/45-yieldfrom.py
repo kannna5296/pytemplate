c9bd11b9caa9c76f39ad53271a636365d9903e90
kannna5296 <kannnahacks5296@gmail.com>
2025-12-14 08:50:17 +0000

    yieldfrom

---
+++ src/6_gen/45-yieldfrom.py
@@ -0,0 +1,35 @@
+def move(period, speed):
+    for _ in range(period):
+        yield speed
+
+def pause(delay):
+    for _ in range(delay):
+        yield 0
+
+def animate():
+    for delta in move(4,5):
+        yield delta
+    for delta in pause(3):
+        yield delta
+    for delta in move(2, 3):
+        yield delta
+
+def render(delta):
+    print(f"Delta: {delta}")
+
+def run(func):
+    for delta in func():
+        render(delta)
+
+print("yieldそのもので書くパターン。")
+run(animate)
+
+
+def animate_composed():
+    yield from move(4,5)
+    yield from pause(3)
+    yield from move(2,3)
+
+print("yieldそのもので書くパターン。")
+run(animate_composed)
+
