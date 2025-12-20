f82023e1710c18e7fef3589be0adc40955a69079
kannna5296 <kannnahacks5296@gmail.com>
2025-12-20 07:59:04 +0000

    オブジェクト指向

---
+++ src/7_class/49-isinstance.py
@@ -0,0 +1,30 @@
+class Integer:
+    def __init__(self, value):
+        self.value = value
+
+class Add:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class Multiply:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+tree = Add(Integer(2),Integer(9)) #AST 抽象構文木 abstract syntac tree
+
+# ツリーウォーキング
+def evaluate(node):
+    if isinstance(node, Integer):
+        return node.value
+    elif isinstance(node, Add):
+        return evaluate(node.left) + evaluate(node.right)
+    elif isinstance(node, Multiply):
+        return evaluate(node.left) * evaluate(node.right)
+    else:
+        raise NotImplementedError
+
+print(evaluate(tree))
+
+
---
+++ src/7_class/49-poly.py
@@ -0,0 +1,35 @@
+class Node:
+    def evaluate(self):
+        raise NotImplementedError
+
+class IntergerNode(Node):
+    def __init__(self, value):
+        self.value = value
+
+    def evaluate(self):
+        return self.value
+
+class AddNode(Node):
+    def __init__(self, right, left):
+        self.left = left
+        self.right = right
+
+    def evaluate(self):
+        return self.left.evaluate() + self.right.evaluate()
+
+class MultiplyNode(Node):
+    def __init__(self, right, left):
+        self.left = left
+        self.right = right
+
+    def evaluate(self):
+        return self.left.evaluate() * self.right.evaluate()
+
+
+tree = MultiplyNode(
+    AddNode(IntergerNode(3), IntergerNode(5)),
+    AddNode(IntergerNode(2), IntergerNode(4)),
+)
+
+print(tree.evaluate())
+
