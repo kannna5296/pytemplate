80055463809bbd893d8d136d63b3e3703ccbc46b
kannna5296 <kannnahacks5296@gmail.com>
2025-12-20 08:08:03 +0000

    poly

--- src/7_class/49-poly.py
+++ src/7_class/49-poly.py
@@ -2,12 +2,18 @@ class Node:
     def evaluate(self):
         raise NotImplementedError
 
+    def pretty(self):
+        raise NotImplementedError
+
 class IntergerNode(Node):
     def __init__(self, value):
         self.value = value
 
     def evaluate(self):
         return self.value
+
+    def pretty(self):
+        return repr(self.value)
 
 class AddNode(Node):
     def __init__(self, right: Node, left: Node):
@@ -17,6 +23,9 @@ class AddNode(Node):
     def evaluate(self):
         return self.left.evaluate() + self.right.evaluate()
 
+    def pretty(self):
+        return f"{self.left.pretty()} + {self.right.pretty()}"
+
 class MultiplyNode(Node):
     def __init__(self, right: Node, left: Node):
         self.left = left
@@ -25,11 +34,15 @@ class MultiplyNode(Node):
     def evaluate(self):
         return self.left.evaluate() * self.right.evaluate()
 
+    def pretty(self):
+        return f"({self.left.pretty()}) * ({self.right.pretty()})"
+
 
 tree = MultiplyNode(
     AddNode(IntergerNode(3), IntergerNode(5)),
     AddNode(IntergerNode(2), IntergerNode(4)),
 )
 
-print(tree.evaluate())
+
+print(f"{tree.pretty()} = {tree.evaluate()}")
 
